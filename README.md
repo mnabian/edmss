586e712f87ef5a520bc790de298e81bb8e5eb44a
Mohammad <mnabian@nvidia.com>
2023-11-22 15:26:20 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# EDM stochastic sampler
+
+LISENCE: Attribution-NonCommercial-ShareAlike 4.0 International License
+
+This is a derivative of the [EDM stochastic sampler](https://github.com/NVlabs/edm),
+tailored for the [RessDiff model](https://github.com/NVIDIA/modulus/tree/main/modulus/experimental/resdiff)
+for [weather data super-resolution](https://arxiv.org/pdf/2309.15214.pdf).
---
+++ setup.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# This work is licensed under a Creative Commons
+# Attribution-NonCommercial-ShareAlike 4.0 International License.
+# You should have received a copy of the license along with this
+# work. If not, see http://creativecommons.org/licenses/by-nc-sa/4.0/
+
+from setuptools import setup, find_packages
+
+setup(
+    name="stochastic-sampler",
+    version="0.1.0",
+    packages=find_packages(),
+)
