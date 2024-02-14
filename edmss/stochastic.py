68ccd7fd92724f9e9fae4dae27b51d05a1a581eb
Tao Ge <115046371+tge25@users.noreply.github.com>
2024-02-14 15:19:56 -0800

    Update stochastic.py

--- edmss/stochastic.py
+++ edmss/stochastic.py
@@ -143,15 +143,19 @@ def edm_sampler(
         x_lr = image_batching(x_lr, img_shape, img_shape, patch_shape, patch_shape, batch_size, overlap_pix, boundary_pix, input_interp)
             
     # Main sampling loop.
-    x_next = latents.to(torch.float64) * t_steps[0]   
-    for i, (t_cur, t_next) in enumerate(zip(t_steps[:-1], t_steps[1:])): # 0, ..., N-1
+    x_next = latents.to(torch.float64) * t_steps[0]
+    for i, (t_cur, t_next) in enumerate(zip(t_steps[:-1], t_steps[1:])):  # 0, ..., N-1
         x_cur = x_next
+
         # Increase noise temporarily.
-        gamma = min(S_churn / num_steps, np.sqrt(2) - 1) if S_min <= t_cur <= S_max else 0
+        gamma = (
+            min(S_churn / num_steps, np.sqrt(2) - 1) if S_min <= t_cur <= S_max else 0
+        )
         t_hat = net.round_sigma(t_cur + gamma * t_cur)
-        x_hat = x_cur + (t_hat ** 2 - t_cur ** 2).sqrt() * S_noise * randn_like(x_cur)
+        x_hat = x_cur + (t_hat**2 - t_cur**2).sqrt() * S_noise * randn_like(x_cur)
+            
         # Euler step.
-        #denoised = net(x_hat, t_hat, class_labels).to(torch.float64)    #x_lr
+        # denoised = net(x_hat, t_hat, class_labels).to(torch.float64)    #x_lr
         
         if (patch_shape!=img_shape):
             x_hat_batch = image_batching(x_hat, img_shape, img_shape, patch_shape, patch_shape, batch_size, overlap_pix, boundary_pix)
