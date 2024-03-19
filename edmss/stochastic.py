c56a7b0143b60b037b451a7d7c5c29511c0ca6a3
Tao Ge <115046371+tge25@users.noreply.github.com>
2024-03-19 13:20:39 -0700

    Update stochastic.py

--- edmss/stochastic.py
+++ edmss/stochastic.py
@@ -5,9 +5,26 @@
 # You should have received a copy of the license along with this
 # work. If not, see http://creativecommons.org/licenses/by-nc-sa/4.0/
 
+# Copyright (c) 2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 import numpy as np
 import torch
 import math
+
+# ruff: noqa: E731
 
 def image_batching(input, 
         img_shape_x, 
@@ -18,12 +35,7 @@ def image_batching(input,
         overlap_pix,
         boundary_pix,
         input_interp=None):
-    '''
-    full image --> batch of patched image
-    batch_size: original batch size without patching
-    overlap_pix: overlapping size between patches
-    boundary_pix: boundary pixel size that will be cropped
-    '''
+
     patch_num_x = math.ceil(img_shape_x/(patch_shape_x-overlap_pix-boundary_pix))
     patch_num_y = math.ceil(img_shape_y/(patch_shape_y-overlap_pix-boundary_pix))
     padded_shape_x = (patch_shape_x-overlap_pix-boundary_pix) * (patch_num_x-1) + patch_shape_x + boundary_pix
@@ -35,7 +47,7 @@ def image_batching(input,
     input_padded = image_padding(input)
     patch_num = patch_num_x*patch_num_y
     if input_interp is not None:
-        output = torch.zeros(patch_num*batch_size, input.shape[1]*2-4, patch_shape_x, patch_shape_y).cuda()
+        output = torch.zeros(patch_num*batch_size, input.shape[1]*2-100, patch_shape_x, patch_shape_y).cuda()
     else:
         output = torch.zeros(patch_num*batch_size, input.shape[1], patch_shape_x, patch_shape_y).cuda() 
     for x_index in range(patch_num_x):
@@ -43,7 +55,7 @@ def image_batching(input,
             x_start = x_index*(patch_shape_x-overlap_pix-boundary_pix) 
             y_start = y_index*(patch_shape_y-overlap_pix-boundary_pix)           
             if input_interp is not None:
-                output[(x_index*patch_num_x+y_index)*batch_size:(x_index*patch_num_x+y_index+1)*batch_size,] = torch.cat((input_padded[:,:,x_start:x_start+patch_shape_x, y_start:y_start+patch_shape_y], input_interp), 1)
+                output[(x_index*patch_num_x+y_index)*batch_size:(x_index*patch_num_x+y_index+1)*batch_size,] = torch.cat((input_padded[:,:,x_start:x_start+patch_shape_x, y_start:y_start+patch_shape_y], input_interp), dim=1)
             else:
                 output[(x_index*patch_num_x+y_index)*batch_size:(x_index*patch_num_x+y_index+1)*batch_size,] = input_padded[:,:,x_start:x_start+patch_shape_x, y_start:y_start+patch_shape_y] 
             #print(x_index, y_index, torch.sum(torch.abs(input[:,:,x_start:x_start+patch_shape_x, y_start:y_start+patch_shape_y])), torch.sum(torch.abs(output)))
@@ -60,10 +72,8 @@ def image_fuse(input,
         boundary_pix
         ):
     '''
-    batch of patched image --> full image
-    batch_size: original batch size without patching
-    overlap_pix: overlapping size between patches
-    boundary_pix: boundary pixel size that will be cropped
+    input: batched forecasts (196,:,64,64)
+    output: padded forecasts (1,:,480,480)
     '''
     patch_num_x = math.ceil(img_shape_x/(patch_shape_x-overlap_pix-boundary_pix))
     patch_num_y = math.ceil(img_shape_y/(patch_shape_y-overlap_pix-boundary_pix))
@@ -97,66 +107,41 @@ def image_fuse(input,
 
 
 def edm_sampler(
-    net,
-    latents,
-    img_lr,
-    class_labels=None,
-    randn_like=torch.randn_like,
-    img_shape = 448, 
-    patch_shape = 448, 
-    overlap_pix = 4, 
-    boundary_pix = 2, 
-    num_steps=18,
-    sigma_min=0.002,
-    sigma_max=800,
-    rho=7,
-    S_churn=0,
-    S_min=0,
-    S_max=float("inf"),
-    S_noise=1,
-):  # num_steps=18, sigma_max=80, igma_min=0.002
+    net, latents, img_lr, patch_shape=448, img_shape=448, mean_hr=None,  overlap_pix = 4, boundary_pix = 2, gridtype = 'sinusoidal',class_labels=None, randn_like=torch.randn_like,
+    num_steps=18, sigma_min=0.002, sigma_max=800, rho=7,
+    S_churn=0, S_min=0, S_max=float('inf'), S_noise=1,
+):   #num_steps=18, sigma_max=80, igma_min=0.002
     # Adjust noise levels based on what's supported by the network.
-    "Proposed EDM sampler (Algorithm 2) with minor changes to enable super-resolution."
     sigma_min = max(sigma_min, net.sigma_min)
-    sigma_max = min(sigma_max, net.sigma_max)
-
+    if gridtype == "learnable":
+        pos_embd = torch.permute(net.model.pos_embd, (2, 1, 0)).expand(img_lr.shape[0], -1, -1, -1)          
+        img_lr = torch.cat((img_lr, pos_embd),dim=1)  
+    #sigma_max = min(sigma_max, net.sigma_max)
     # Time step discretization.
     step_indices = torch.arange(num_steps, dtype=torch.float64, device=latents.device)
-    t_steps = (
-        sigma_max ** (1 / rho)
-        + step_indices
-        / (num_steps - 1)
-        * (sigma_min ** (1 / rho) - sigma_max ** (1 / rho))
-    ) ** rho
-    t_steps = torch.cat(
-        [net.round_sigma(t_steps), torch.zeros_like(t_steps[:1])]
-    )  # t_N = 0
-
-    # conditioning
-    x_lr = img_lr
+    t_steps = (sigma_max ** (1 / rho) + step_indices / (num_steps - 1) * (sigma_min ** (1 / rho) - sigma_max ** (1 / rho))) ** rho
+    t_steps = torch.cat([net.round_sigma(t_steps), torch.zeros_like(t_steps[:1])]) # t_N = 0
+    #conditioning
+    
     batch_size = img_lr.shape[0]
-        
-    # global map interpolation
     input_interp = torch.nn.functional.interpolate(img_lr[:,0:12], (patch_shape, patch_shape), mode='bilinear') 
-
-    # input batching
+    x_lr = torch.cat((img_lr[:,0:12],img_lr[:,16:]), axis=1)
+    
+    # input padding
     if (patch_shape!=img_shape):
         x_lr = image_batching(x_lr, img_shape, img_shape, patch_shape, patch_shape, batch_size, overlap_pix, boundary_pix, input_interp)
-            
+        mean_hr = image_batching(mean_hr, img_shape, img_shape, patch_shape, patch_shape, batch_size, overlap_pix, boundary_pix).expand(x_lr.shape[0], -1, -1, -1)
+        x_lr = torch.cat((mean_hr, x_lr), dim=1)
     # Main sampling loop.
-    x_next = latents.to(torch.float64) * t_steps[0]
-    for i, (t_cur, t_next) in enumerate(zip(t_steps[:-1], t_steps[1:])):  # 0, ..., N-1
+    x_next = latents.to(torch.float64) * t_steps[0]   
+    for i, (t_cur, t_next) in enumerate(zip(t_steps[:-1], t_steps[1:])): # 0, ..., N-1
         x_cur = x_next
-
         # Increase noise temporarily.
-        gamma = (
-            min(S_churn / num_steps, np.sqrt(2) - 1) if S_min <= t_cur <= S_max else 0
-        )
+        gamma = min(S_churn / num_steps, np.sqrt(2) - 1) if S_min <= t_cur <= S_max else 0
         t_hat = net.round_sigma(t_cur + gamma * t_cur)
-        x_hat = x_cur + (t_hat**2 - t_cur**2).sqrt() * S_noise * randn_like(x_cur)
-            
-        # Euler step. Perform patching operation on score tensor if patch-based generation is used
-        # denoised = net(x_hat, t_hat, class_labels).to(torch.float64)    #x_lr
+        x_hat = x_cur + (t_hat ** 2 - t_cur ** 2).sqrt() * S_noise * randn_like(x_cur)
+        # Euler step.
+        #denoised = net(x_hat, t_hat, class_labels).to(torch.float64)    #x_lr
         
         if (patch_shape!=img_shape):
             x_hat_batch = image_batching(x_hat, img_shape, img_shape, patch_shape, patch_shape, batch_size, overlap_pix, boundary_pix)
