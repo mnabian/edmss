9d7036c13b1a49ba588e59eb4a08a694cc8ec2ac
Tao Ge <115046371+tge25@users.noreply.github.com>
2024-03-19 15:00:09 -0700

    Update stochastic.py

--- edmss/stochastic.py
+++ edmss/stochastic.py
@@ -121,7 +121,7 @@ def edm_sampler(
     # input padding
     if (patch_shape!=img_shape):
         x_lr = image_batching(x_lr, img_shape, img_shape, patch_shape, patch_shape, batch_size, overlap_pix, boundary_pix, input_interp)
-        if mean_hr:
+        if mean_hr is not None:
                 mean_hr = image_batching(mean_hr, img_shape, img_shape, patch_shape, patch_shape, batch_size, overlap_pix, boundary_pix).expand(x_lr.shape[0], -1, -1, -1)
                 x_lr = torch.cat((mean_hr, x_lr), dim=1)
     # Main sampling loop.
