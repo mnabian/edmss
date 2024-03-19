9b67c94844ee3853f286ea508e7720374ded10cd
Tao Ge <115046371+tge25@users.noreply.github.com>
2024-03-19 14:10:41 -0700

    Update stochastic.py

--- edmss/stochastic.py
+++ edmss/stochastic.py
@@ -113,8 +113,7 @@ def edm_sampler(
     input_interp = torch.nn.functional.interpolate(img_lr[:,0:12], (patch_shape, patch_shape), mode='bilinear') 
     # 12 input variable + 4 positional embedding, need to optimize
 
-    print(img_lr.shape[0])
-    if img_lr.shape[0] != 16:    
+    if img_lr.shape[1] != 16:    
         x_lr = torch.cat((img_lr[:,0:12],img_lr[:,16:]), axis=1)
     else:
         x_lr = img_lr
